3a7783969855fa58a93fa20529de11ee5f2e5a84
Andrey Bespalov <streloc84@gmail.com>
2021-01-02 13:59:18 +0400

    start

---
+++ src/pages/login/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import '/home/streloc84/Рабочий стол/notes/src/App.css';
+
+class Login extends React.Component {
+    render(){
+    return(
+    <div className="App">
+      <header className="App-header">
+               <h1>Логин</h1>
+      </header>
+      <body>
+
+      </body>
+      <footer>
+
+      </footer>
+    </div>
+    )
+    }
+}
+
+export default Login;
